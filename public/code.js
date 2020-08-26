ba737518d9f8ac45da21bb5486889f6cf6a7734e
funfun313 <43829418+funfun313@users.noreply.github.com>
2020-08-25 19:16:35 -0500

    make pictures show up! :+1:

--- public/code.js
+++ public/code.js
@@ -2,6 +2,23 @@ const storage = firebase.app().storage();
 const storageRef = storage.ref();
 
 const f = document.getElementById("myFile");
+
+window.onload = function(){
+    storageRef.listAll().then(function(result){
+        console.log(result.items);
+        // set up html to recieve image info
+        const photodiv = document.getElementById("photodisplay");
+        photodiv.innerHTML = "";
+        // loop through the images
+        result.items.forEach(function(img){
+            img.getDownloadURL().then(function(url){
+                // write some html
+                photodiv.innerHTML = photodiv.innerHTML + "<div class='photo'><span class= 'author'>Wonder Woman</span><img src = '" + url + "' width = '200px'><span class-'caption'>what an amazing photo :)</span></div>";
+            })
+        })
+    })
+}
+
 function upload(){
     console.log("starting to store file...");
     let imgname = f.files[0]["name"];
