1aa7692a5181e3ccfd18bd9f0ac16e14f8072c6f
funfun313 <43829418+funfun313@users.noreply.github.com>
2020-09-22 19:44:57 -0500

    Worked on automatic uploads!!

--- public/code.js
+++ public/code.js
@@ -6,28 +6,48 @@ const postsRef = dbRef.ref('posts');
 
 const f = document.getElementById("myFile");
 
+
+const appAuth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
 window.onload = function(){
 
     postsRef.once("value").then(function(snapshot){
-        // set up html to recieve image info
-        const photodiv = document.getElementById("photodisplay");
-        photodiv.innerHTML = "";
-        
+        //displayAllPhotos(snapshot);
+    });
+    
+}
 
-        snapshot.forEach(function(childSnapshot){
+// this function listens for changes to the database
+postsRef.on("value", function(snapshot){
+    console.log(snapshot);
+    displayAllPhotos(snapshot);
+})
+
+function displayAllPhotos(snapshot){
+
+    // set up html to recieve image info
+    const photodiv = document.getElementById("photodisplay");
+    photodiv.innerHTML = "";
+    
+    snapshot.forEach(function(catSnapshot){
+        catSnapshot.forEach(function(childSnapshot){
             console.log(childSnapshot.val());
+            console.log(childSnapshot.val().img_pth);
             let img = storageRef.child(childSnapshot.val().img_pth);
+            console.log("set img");
             img.getDownloadURL().then(function(url){
-                 // write some html
+                    // write some html
+                    console.log("right before the html");
                 photodiv.innerHTML = photodiv.innerHTML + "<div class='photo'><div class= 'author'>"+ childSnapshot.val().author +  "</div><img src = '" + url + "' width = '200px'><div class='caption'>"+ childSnapshot.val().caption +"</div></div>";
             })
 
                     
         })
-    })
-    
-}
 
+    })
+
+}
 function upload(){
     console.log("starting to store file...");
     let imgname = f.files[0]["name"];
@@ -49,7 +69,7 @@ function savePicInfo(img){
     let category = "";
     for(let i = 0; i < radiobtn_list.length; i++){
         let radiobtn = radiobtn_list[i];
-        if(radiobtn.selected){
+        if(radiobtn.checked){
             category = radiobtn.value;
         }
     }
@@ -57,7 +77,7 @@ function savePicInfo(img){
     console.log(img);
     console.log(document.getElementById("captiontext").value);
     
-    postsRef.child(cateogry).push( {
+    postsRef.child(category).push( {
             author: 'AUTHOR',
             caption: document.getElementById("captiontext").value,
             img_pth: img,
